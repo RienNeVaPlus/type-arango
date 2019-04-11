9485c8ff51f3c753f4b3250ecff43c4e6dfe8ab4
RienNeVaPlus <nikolai@nikolailang.com>
2019-04-11 23:38:50 +0200

    v0.3.1

--- src/utils/arango.ts
+++ src/utils/arango.ts
@@ -1,4 +1,4 @@
 import {isFoxx} from "../utils";
 
-export const arango = isFoxx() ? require('@arangodb') : new (require('arangojs'));
+export const arango = isFoxx() ? require('@arangodb') : null;
 export const db = isFoxx() ? arango.db : null;
