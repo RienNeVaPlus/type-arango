0b5865a295ba70145336297e4ae280ec52c535f2
RienNeVaPlus <nikolai@nikolailang.com>
2019-07-06 01:37:37 +0200

    Fixed bug in Type.I18n#forClient

--- src/models/Type.models.ts
+++ src/models/Type.models.ts
@@ -4,7 +4,8 @@ export namespace Type {
 
 		static _typeArango: string = '0.4';
 		static forClient(val: any, {req,session}: any){
-			const locale = req.param('locale') || session().data.locale || 'en';
+			const sess = session();
+			const locale = req.param('locale') || sess.data ? sess.data.locale : 'en';
 			return val[locale] || val[locale.substr(0,2)] || null;
 		}
 	}
