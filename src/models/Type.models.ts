21b575c2a930bbbcf9351bdf6d1342ae275f7bdb
RienNeVaPlus <nikolai@nikolailang.com>
2019-07-06 01:46:48 +0200

    Fixed session locale in Type.I18n

--- src/models/Type.models.ts
+++ src/models/Type.models.ts
@@ -5,7 +5,7 @@ export namespace Type {
 		static _typeArango: string = '0.4';
 		static forClient(val: any, {req,session}: any){
 			const sess = session();
-			const locale = req.param('locale') || sess.data ? sess.data.locale : 'en';
+			const locale = req.param('locale') || (sess.data ? sess.data.locale : 'en');
 			return val[locale] || val[locale.substr(0,2)] || null;
 		}
 	}
