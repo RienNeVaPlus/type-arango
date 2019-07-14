e593d7aee9fe3c222074e11120b2717be55a837a
RienNeVaPlus <nikolai@nikolailang.com>
2019-07-14 08:11:40 +0200

    Allow unknown object keys in Type.I18n

--- src/models/Type.models.ts
+++ src/models/Type.models.ts
@@ -1,10 +1,14 @@
 import {config} from "../index";
+import * as Joi from 'joi'
 
 export namespace Type {
 	export class I18n {
 		[key: string]: string | any
 
 		static _typeArango: string = '0.4';
+
+		static schema: Joi.ObjectSchema = Joi.object().unknown();
+
 		static forClient(val: any, {req,session}: any){
 			const sess = session();
 			const param = req.param('locale');
